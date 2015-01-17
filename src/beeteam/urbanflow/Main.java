1622848239cc77803678c818f0aa55d980705503
Augustin Delale <adelale@hotmail.com>
2015-01-17 21:10:54 +0100

    ça bosse, ça bosse...

--- src/beeteam/urbanflow/Main.java
+++ src/beeteam/urbanflow/Main.java
@@ -3,9 +3,12 @@ package beeteam.urbanflow;
 import beeteam.urbanflow.fte.GameEngine.GameMode;
 
 public class Main {
+	
+	
 
     public static void main(String[] args) {
-	new UrbanFlow().gameLoop(GameMode.ARENA, BeeTeamData.BOT_TOKEN, BeeTeamData.BOT_SECRET);
+	
+    	new UrbanFlow().gameLoop(GameMode.ARENA, BeeTeamData.BOT_TOKEN, BeeTeamData.BOT_SECRET);
     }
 
 }
