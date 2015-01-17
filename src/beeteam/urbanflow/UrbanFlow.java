60aea4033ba48dda40f00a7c1ade17121deb0f6e
FTE <f.tetu@ees-assurance.fr>
2015-01-17 21:09:28 +0100

    Chg interface

--- src/beeteam/urbanflow/UrbanFlow.java
+++ src/beeteam/urbanflow/UrbanFlow.java
@@ -3,13 +3,14 @@ package beeteam.urbanflow;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import beeteam.urbanflow.fte.GameEngine;
 
 public class UrbanFlow extends GameEngine {
 
     @Override
-    protected List<Moveset> prepareNavigation(long firstStopId, long targetStopId, Date date) {
+    protected List<Moveset> prepareNavigation(long firstStopId, long targetStopId, Date date, @SuppressWarnings("rawtypes") Map incidents) {
 	List<Moveset> moves = new ArrayList<Moveset>();
 	// TODO
 	return moves;
