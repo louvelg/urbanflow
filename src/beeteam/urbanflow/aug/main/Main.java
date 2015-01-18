31bbd789a9fef8c074fed33c14e2f2b89e11d796
Augustin Delale <adelale@hotmail.com>
2015-01-18 09:17:12 +0100

    blabla

--- src/beeteam/urbanflow/aug/main/Main.java
+++ src/beeteam/urbanflow/aug/main/Main.java
@@ -6,7 +6,7 @@ import beeteam.urbanflow.fte.GameEngine.GameMode;
 public class Main {
 
 	public static void main(String[] args) {
-		new UrbanFlow().gameLoop(GameMode.TRAINING, BeeTeamData.BOT_TOKEN, BeeTeamData.BOT_SECRET);
+		new UrbanFlow().gameLoop(GameMode.ARENA, BeeTeamData.BOT_TOKEN, BeeTeamData.BOT_SECRET);
 	}
 
 }
